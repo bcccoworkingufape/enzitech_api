b5625d5a4c5efa3e9199f6784ffc62bbb352d8b6
Guilherme Felix <guilhermerutemfelix@gmail.com>
2026-06-14 18:42:49 -0300

    feat: initialize project structure

---
+++ src/main/java/br/edu/ufape/enzitech/EnzitechApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.enzitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnzitechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnzitechApplication.class, args);
+	}
+
+}
